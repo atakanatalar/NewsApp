d17fcc40d0b9707654ddd7ea71acc0af4f97f278
Atakan Atalar <atakanatalar@icloud.com>
2024-10-18 13:24:01 +0300

    Initialize project

---
+++ NewsApp/NewsApp/Scenes/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Atakan Atalar on 18.10.2024.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .secondarySystemBackground
+        
+        title = "News App"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
