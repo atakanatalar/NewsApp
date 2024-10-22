73b6c666aa53a0d9f7f360ac3e811b38ea492d8a
Atakan Atalar <atakanatalar@icloud.com>
2024-10-22 14:36:35 +0300

    Refactor moved business logic from HomeViewController to HomeViewModel

--- NewsApp/NewsApp/Scenes/HomeViewController.swift
+++ NewsApp/NewsApp/Scenes/HomeViewController.swift
@@ -8,108 +8,95 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    let networkManager = NetworkManager()
-    var articles: [Article] = []
-    let tableView = UITableView()
     let searchController = UISearchController()
-    
-    var selectedCategory: NewsCategory = .general
-    var currentPage: Int = 1
-    var totalResults: Int = 0
-    var isLoading: Bool = false
-    
+    let customSegmentedControl = CustomSegmentedControl(segmentTitles: NewsCategory.allCases.map { $0.description })
+    let tableView = UITableView()
     let activityIndicator = UIActivityIndicatorView(style: .medium)
     
-    let customSegmentedControl = CustomSegmentedControl(segmentTitles: NewsCategory.allCases.map { $0.description })
+    private var viewModel = HomeViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        viewModel.delegate = self
+        setupUI()
+        viewModel.fetchNews()
+    }
+    
+    private func setupUI() {
         view.backgroundColor = .secondarySystemBackground
-        
         title = "News App"
         navigationController?.navigationBar.prefersLargeTitles = true
         
+        setupSearchController()
+        setupSegmentedControl()
+        setupTableView()
+        setupActivityIndicator()
+    }
+    
+    private func setupSearchController() {
+        searchController.delegate = self
         searchController.searchResultsUpdater = self
         searchController.searchBar.placeholder = "Search"
         searchController.obscuresBackgroundDuringPresentation = false
         navigationItem.searchController = searchController
-        
+    }
+    
+    private func setupSegmentedControl() {
         customSegmentedControl.buttonTapped = { [weak self] index in
             guard let self = self else { return }
-            self.selectedCategory = NewsCategory.allCases[index]
-            self.filterNews(by: self.selectedCategory)
+            self.viewModel.selectedCategory = NewsCategory.allCases[index]
         }
         
         customSegmentedControl.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 35)
-        
+    }
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(NewsTableViewCell.self, forCellReuseIdentifier: "NewsCell")
         tableView.backgroundColor = .secondarySystemBackground
         tableView.tableHeaderView = customSegmentedControl
-        
-        view.addSubview(tableView)
         tableView.frame = view.bounds
-        
+    }
+    
+    private func setupActivityIndicator() {
+        view.addSubview(activityIndicator)
         activityIndicator.center = view.center
         activityIndicator.color = .systemBlue
         activityIndicator.hidesWhenStopped = true
-        view.addSubview(activityIndicator)
-        
-        fetchNews()
     }
-    
-    func filterNews(by category: NewsCategory) {
-        selectedCategory = category
-        currentPage = 1
-        articles.removeAll()
-        fetchNews(page: currentPage)
-    }
-    
-    func fetchNews(page: Int = 1, pageSize: Int = 20) {
-        let endpoint: NewsAPIEndpoint
-        
-        if let searchText = navigationItem.searchController?.searchBar.text, !searchText.isEmpty {
-            endpoint = .everything(query: searchText, page: page, pageSize: pageSize)
-        } else {
-            endpoint = .topHeadlines(category: selectedCategory, page: page, pageSize: pageSize)
+}
+
+extension HomeViewController: HomeViewModelDelegate {
+    func didUpdateArticles() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+            self.activityIndicator.stopAnimating()
         }
-        
-        isLoading = true
-        activityIndicator.startAnimating()
-        
-        networkManager.request(endpoint: endpoint) { [weak self] (result: Result<NewsResponse, NetworkError>) in
-            guard let self = self else { return }
-            
-            switch result {
-            case .success(let newsResponse):
-                self.totalResults = newsResponse.totalResults
-                self.articles.append(contentsOf: newsResponse.articles)
-                
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-                    self.isLoading = false
-                    self.activityIndicator.stopAnimating()
-                }
-            case .failure(let error):
-                print(error.localizedDescription)
-                self.isLoading = false
-                DispatchQueue.main.async {
-                    self.activityIndicator.stopAnimating()
-                }
-            }
+    }
+    
+    func didStartLoading() {
+        DispatchQueue.main.async {
+            self.activityIndicator.startAnimating()
+        }
+    }
+    
+    func didFailLoading(error: NetworkError) {
+        print(error.localizedDescription)
+        DispatchQueue.main.async {
+            self.activityIndicator.stopAnimating()
         }
     }
 }
 
 extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return articles.count
+        return viewModel.articles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard articles.indices.contains(indexPath.row) else {
+        guard viewModel.articles.indices.contains(indexPath.row) else {
             return UITableViewCell()
         }
         
@@ -117,7 +104,7 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
             return UITableViewCell()
         }
         
-        let article = articles[indexPath.row]
+        let article = viewModel.articles[indexPath.row]
         cell.configure(with: article)
         cell.selectionStyle = .none
         
@@ -125,8 +112,7 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedArticle = articles[indexPath.row]
-        
+        let selectedArticle = viewModel.articles[indexPath.row]
         let detailVC = DetailViewController()
         detailVC.article = selectedArticle
         navigationController?.pushViewController(detailVC, animated: true)
@@ -143,28 +129,30 @@ extension HomeViewController: UIScrollViewDelegate {
         let contentHeight = scrollView.contentSize.height
         
         if offsetY > contentHeight - scrollView.frame.size.height {
-            loadMoreNews()
-        }
-    }
-    
-    func loadMoreNews() {
-        if !isLoading && articles.count < totalResults {
-            currentPage += 1
-            fetchNews(page: currentPage)
+            viewModel.loadMoreNews()
         }
     }
 }
 
 extension HomeViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
-        articles.removeAll()
-        currentPage = 1
-        fetchNews(page: currentPage)
+        let searchText = searchController.searchBar.text
+        viewModel.resetArticles()
+        viewModel.fetchNews(query: searchText)
+    }
+}
+
+extension HomeViewController: UISearchControllerDelegate {
+    func willPresentSearchController(_ searchController: UISearchController) {
+        tableView.tableHeaderView = nil
+    }
+    
+    func willDismissSearchController(_ searchController: UISearchController) {
+        tableView.tableHeaderView = customSegmentedControl
     }
 }
 
 #Preview {
     let vc = HomeViewController()
-    vc.articles = NewsResponse.mock.articles
     return UINavigationController(rootViewController: vc)
 }
---
+++ NewsApp/NewsApp/ViewModels/HomeViewModel.swift
@@ -0,0 +1,72 @@
+//
+//  HomeViewModel.swift
+//  NewsApp
+//
+//  Created by Atakan Atalar on 22.10.2024.
+//
+
+import Foundation
+
+protocol HomeViewModelDelegate: AnyObject {
+    func didUpdateArticles()
+    func didStartLoading()
+    func didFailLoading(error: NetworkError)
+}
+
+class HomeViewModel {
+    private let networkManager = NetworkManager()
+    var articles: [Article] = []
+    var currentPage: Int = 1
+    var totalResults: Int = 0
+    var isLoading: Bool = false
+    
+    weak var delegate: HomeViewModelDelegate?
+    
+    var selectedCategory: NewsCategory = .general {
+        didSet {
+            resetArticles()
+            fetchNews()
+        }
+    }
+    
+    func resetArticles() {
+        currentPage = 1
+        articles.removeAll()
+        delegate?.didUpdateArticles()
+    }
+    
+    func loadMoreNews() {
+        if !isLoading && articles.count < totalResults {
+            currentPage += 1
+            fetchNews(page: currentPage)
+        }
+    }
+    
+    func fetchNews(page: Int = 1, pageSize: Int = 20, query: String? = nil) {
+        let endpoint: NewsAPIEndpoint
+        
+        if let searchText = query, !searchText.isEmpty {
+            endpoint = .everything(query: searchText, page: page, pageSize: pageSize)
+        } else {
+            endpoint = .topHeadlines(category: selectedCategory, page: page, pageSize: pageSize)
+        }
+        
+        isLoading = true
+        delegate?.didStartLoading()
+        
+        networkManager.request(endpoint: endpoint) { [weak self] (result: Result<NewsResponse, NetworkError>) in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let newsResponse):
+                self.totalResults = newsResponse.totalResults
+                self.articles.append(contentsOf: newsResponse.articles)
+                self.isLoading = false
+                self.delegate?.didUpdateArticles()
+            case .failure(let error):
+                self.isLoading = false
+                self.delegate?.didFailLoading(error: error)
+            }
+        }
+    }
+}
